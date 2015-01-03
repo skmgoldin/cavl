be6cbc6249528467259c1fdb976ab11d0ca56c75
Mike Goldin <skmgoldin@gmail.com>
2015-01-02 16:28:51 -0800

    Include guards.

--- avltree.h
+++ avltree.h
@@ -1,3 +1,6 @@
+#ifndef AVLTREE_H
+#define AVLTREE_H
+
 struct Handle {
   struct Node *root;
   int *comparator (void *, void *); 
@@ -14,3 +17,5 @@ struct Handle *gettree(int (*comparator)(void *, void *), void *data);
 struct Node *addnode(struct Handle *handle, void *data);
 
 int killtree(struct Handle *tree);
+
+#endif /* AVLTREE_H */
