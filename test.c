de6738b7ca9deee1fcf99c572c481f0ab35b2c25
Mike Goldin <skmgoldin@gmail.com>
2015-01-02 17:43:34 -0800

    test program builds, but expect a seg fault in gettree.

--- test.c
+++ test.c
@@ -1,8 +1,24 @@
 #include "avltree.h"
 #include <assert.h>
+#include <stdio.h>
 
+int intcomparator(void *a, void *b) {
+  int inta = *((int *) a);
+  int intb = *((int *) b);
+
+  if(inta == intb) {return 0;}
+
+  return 1;
+}
 int main(int argc, char **argv) {
 
-  assert(gettree(strcmp(), (void *) *(argv)));  
+  printf("%s\n", "Building tree");
+  struct Handle *tree = gettree(&intcomparator, (void *) *(argv));
+  printf("%s\n", "Tree built.");
+
+  printf("%s\n", "Deallocating tree.");
+  killtree(tree);
+  printf("%s\n", "Tree deallocated.");
+
   return 1;
 }
