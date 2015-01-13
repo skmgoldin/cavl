f0272aa62cfc32d75b0c905ce21ca66864c4e814
Michael Goldin <Mike@Michaels-MBP.home>
2015-01-13 16:51:19 -0500

    Improved organization.

--- avltree.h
+++ avltree.h
@@ -3,6 +3,7 @@
 
 #include "avltree.h"
 
+/* Data Structures */
 struct Handle {
   struct Node *root;
   int (*comparator)(void *, void *); 
@@ -14,10 +15,15 @@ struct Node {
   struct Node *rchild;
 };
 
+/* Public Functions */
 struct Handle *gettree(int (*comparator)(void *, void *), void *data);
 
 struct Node *addnode(struct Handle *handle, void *data);
 
 int killtree(struct Handle *tree);
+
+/* Private Functions */
+struct Node *placenode(struct Node *root, struct Node *newnode,
+                       int (*comparator)(void *, void *));
 
 #endif /* AVLTREE_H */
