a550401ea101be86c31b691885be893449f5fd25
Mike Goldin <skmgoldin@gmail.com>
2015-01-19 16:37:42 -0500

    More tests

--- test.sh
+++ test.sh
@@ -10,4 +10,37 @@ echo --------------------
 echo single rotation
 echo --------------------
 
-valgrind --leak-check=yes -q ./treetest 1 2 3
+valgrind --leak-check=yes ./treetest 1 2 3
+
+echo --------------------
+echo double rotation
+echo --------------------
+
+valgrind --leak-check=yes ./treetest 1 3 2
+
+echo --------------------
+echo single rotation, double rotation
+echo --------------------
+
+valgrind --leak-check=yes ./treetest 1 2 3 5 4
+
+echo --------------------
+echo double rotation, single rotation
+echo --------------------
+
+valgrind --leak-check=yes ./treetest 1 3 2 4 5
+
+echo --------------------
+echo single rotation, single rotation
+echo --------------------
+
+valgrind --leak-check=yes ./treetest 1 2 3 4 5
+
+echo --------------------
+echo double rotation, double rotation
+echo --------------------
+
+valgrind --leak-check=yes ./treetest 1 3 2 5 4 
+
+
+
