d9ea6fca99233a7a422d8a445242a2915a9ccbd2
Mike Goldin <skmgoldin@gmail.com>
2015-01-19 16:25:32 -0500

    Makes and tests a single rotation.

--- test.sh
+++ test.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-echo hi
+echo --------------------
+echo Making 
+echo --------------------
+
+make all
+
+echo --------------------
+echo single rotation
+echo --------------------
+
+valgrind --leak-check=yes -q ./treetest 1 2 3
