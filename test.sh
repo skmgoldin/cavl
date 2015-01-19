7634a6cf81f490c5d25712c7ef1f38e3701292dc
Mike Goldin <skmgoldin@gmail.com>
2015-01-19 16:44:23 -0500

    More tests

--- test.sh
+++ test.sh
@@ -7,40 +7,57 @@ echo --------------------
 make all
 
 echo --------------------
-echo single rotation
+echo RS single rotation
 echo --------------------
 
 valgrind --leak-check=yes ./treetest 1 2 3
 
 echo --------------------
-echo double rotation
+echo LS single rotation
+echo --------------------
+
+valgrind --leak-check=yes ./treetest 3 2 1 
+
+echo --------------------
+echo RS double rotation
 echo --------------------
 
 valgrind --leak-check=yes ./treetest 1 3 2
 
 echo --------------------
-echo single rotation, double rotation
+echo LS double rotation
+echo --------------------
+
+valgrind --leak-check=yes ./treetest 5 3 4
+
+echo --------------------
+echo RS single rotation, RS double rotation
 echo --------------------
 
 valgrind --leak-check=yes ./treetest 1 2 3 5 4
 
 echo --------------------
-echo double rotation, single rotation
+echo RS double rotation, RS single rotation
 echo --------------------
 
 valgrind --leak-check=yes ./treetest 1 3 2 4 5
 
 echo --------------------
-echo single rotation, single rotation
+echo RS single rotation, RS single rotation
 echo --------------------
 
 valgrind --leak-check=yes ./treetest 1 2 3 4 5
 
 echo --------------------
-echo double rotation, double rotation
+echo RS double rotation, RS double rotation
 echo --------------------
 
 valgrind --leak-check=yes ./treetest 1 3 2 5 4 
 
+echo --------------------
+echo Random String
+echo --------------------
+
+valgrind --leak-check=yes ./treetest 1 3 2 5 4 8 3 19 4 1 4 19 4191 4 1 4 23 2 1 3 13 4 1 4 184 8 8 8 32 1 3 1 9 
 
 
