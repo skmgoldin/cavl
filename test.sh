0dc3c916c6f50d59457cb58beadf34fd4fcbbcc2
Mike Goldin <skmgoldin@gmail.com>
2015-01-20 20:09:49 -0500

    Added a test.

--- test.sh
+++ test.sh
@@ -84,11 +84,18 @@ eval $PRINTING
 $CLEANUP
 
 echo --------------------
-echo Random String
+echo Random String 1
 echo --------------------
 
 $VALGRIND $TESTPROG 1 2 3 1 3 2 5 4 8 3 9 4 1 4 1 1 9 4 1 4 3 2 1 3 3 4 1 4 1 8 8 8 3 1 3 1 9 3 2 9 8 4 1 9 1 3 4 3 1 2 3 2 3 4 5 6 7 2 3 4 8 7 2 1 3 4 5 3 1 9 1 1 4 5 3 3 1 1 4 5 8 3 3 4 2 2 1 3 1 4 2 1 2 3 2 1 2 2 1 2 2 2 2 3 4 2 3 9 5 2 4 6 2 4 3 4 9 4 7 1 7 7 2 3 8 6 4 3 4 2 1 9 4 2 8 1 2 3 8 9 4 5 3 2 2 1 8 4 9 9 1 3 4 2 1 2 3 2 1 3 9 9 8 7 5 8 6 9 3 2 1 9 5 6 9
 eval $PRINTING
 $CLEANUP
 
+echo --------------------
+echo Random String 2 --CPU intensive, keep waiting--
+echo --------------------
+
+$VALGRIND $TESTPROG 1000
+eval $PRINTING
+$CLEANUP
 
