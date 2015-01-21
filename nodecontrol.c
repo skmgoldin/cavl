7950a40e6803d4979be61b9713dc5d92c1a7f4f2
Mike Goldin <skmgoldin@gmail.com>
2015-01-21 18:18:39 -0500

    Comments.

--- nodecontrol.c
+++ nodecontrol.c
@@ -1,6 +1,11 @@
 #include "balancecontrol.h"
 #include <stdio.h>
 
+/********************
+ * Insert a node.
+ * Recursive insertion method adds *newnode to the subtree *root and returns
+ * the tree's new root after rebalancing.
+ ********************/
 struct Node *insert(struct Node *root, struct Node *newnode,
                     int (*comparator)(void *, void *)) {
 
--- nodecontrol.h
+++ nodecontrol.h
@@ -1,4 +1,9 @@
 #include "datastructures.h"
 
+/********************
+ * Insert a node.
+ * Recursive insertion method adds *newnode to the subtree *root and returns
+ * the tree's new root after rebalancing.
+ ********************/
 struct Node *insert(struct Node *root, struct Node *newnode,
                     int (*comparator)(void *, void *));
