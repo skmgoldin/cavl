659aea9601e150fb3de3aa1d1e342bfb40d0188b
Mike Goldin <skmgoldin@gmail.com>
2015-01-22 15:06:56 -0500

    Added cases for not-found and failure.

--- traversal.c
+++ traversal.c
@@ -1,12 +1,19 @@
-struct Node *getnode(struct Node *root, struct Node *data,
+#include "traversal.h"
+#include <stdio.h>
+
+struct Node *getnode(struct Node *root, void *data,
                      int (*comparator)(void *, void *)) {
 
-  int cmpval = comparator(root->data, newnode->data);
+  int cmpval = comparator(root->data, data);
 
   if(cmpval == 0) {
     return root;
   }
       
+  if(cmpval != 0 && root->lchild == NULL && root->rchild == NULL) {
+    return NULL;
+  }
+
   if(cmpval == -1) {
     return getnode(root->lchild, data, comparator);
   }
@@ -15,4 +22,6 @@ struct Node *getnode(struct Node *root, struct Node *data,
     return getnode(root->rchild, data, comparator);
   }
 
+  fprintf(stderr, "%s\n", "find() failed.");
+  exit(1);
 }
